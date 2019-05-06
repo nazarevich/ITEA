4c580e9ba4513e1524f285b7c1aa1fee72650a92
Nazarevich <Nazarevich@NAZAREVICH-PC>
2019-05-06 22:00:31 +0300

    Exercise - 5 Started

---
+++ ConsoleApp1/Exercice-5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter_radius");
+            int radius;
+
+        }
+    }
+}
